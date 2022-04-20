24d7ea8d1eed1f9e2e4667fd08837da0666c3ed4
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2022-04-20 16:53:57 +0700

    NEW: tranfer data 1-1(A->B & B->A)

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const message = localStorage.getItem("data") ?? `{}`;
-const tempMess = Object.assign(JSON.parse(message));
+let tempMess = Object.assign(JSON.parse(message));
 const elemChatA = document.getElementById("chatA");
 const input = document.querySelector("input");
 const elemScreen = document.getElementById("screen");
 
-let postMessage = (value) => {
+let sendMessage = (value) => {
   let elemDiv = document.createElement("p");
   elemDiv.setAttribute("class", "col  message");
   //   elemDiv.setAttribute("style", "display: inline-block");
@@ -13,23 +13,33 @@ let postMessage = (value) => {
 };
 let getMessage = () => {
   for (let key in tempMess) {
-    postMessage(tempMess[key]);
+    sendMessage(tempMess[key][0]);
   }
 };
 getMessage();
 
 let updateValue = (e) => {
   let eValue = e.target.value;
-  postMessage(eValue);
-  tempMess[Object.keys(tempMess).length] = eValue;
+  sendMessage(eValue);
+  tempMess[Object.keys(tempMess).length] = [eValue, "isA"];
   localStorage.setItem("data", JSON.stringify(tempMess));
   elemScreen.scrollTop = elemScreen.scrollHeight;
-  var popup = window.open("http://127.0.0.1:8080/chatB.html", "window-2");
-  popup.postMessage(eValue, "http://127.0.0.1:8080/chatB.html");
   e.target.value = "";
+  sendMessage(eValue);
 };
 input.addEventListener("change", updateValue);
 
 let Reset = () => {
   localStorage.clear();
+  location.reload();
+};
+window.addEventListener("message", function (event) {
+  if (event.data != "") {
+    sendMessage(event.data);
+  }
+});
+
+let sendMessage = (value) => {
+  var popup = window.open("http://127.0.0.1:8080/chatB.html", "index");
+  popup.postMessage(value, "http://127.0.0.1:8080/chatB.html");
 };
--- appB.js
+++ appB.js
@@ -1,11 +1,10 @@
 const message = localStorage.getItem("data") ?? `{}`;
-const tempMess = Object.assign(JSON.parse(message));
-
+let tempMess = Object.assign(JSON.parse(message));
 const elemChatA = document.getElementById("chatA");
 const input = document.querySelector("input");
 const elemScreen = document.getElementById("screen");
 
-let postMessage = (value) => {
+let sendMessage = (value) => {
   let elemDiv = document.createElement("p");
   elemDiv.setAttribute("class", "col  message");
   //   elemDiv.setAttribute("style", "display: inline-block");
@@ -14,26 +13,33 @@ let postMessage = (value) => {
 };
 let getMessage = () => {
   for (let key in tempMess) {
-    postMessage(tempMess[key]);
+    sendMessage(tempMess[key][0]);
   }
 };
 getMessage();
 
 let updateValue = (e) => {
-  postMessage(e.target.value);
-  e.target.value = "";
+  let eValue = e.target.value;
+  sendMessage(eValue);
+  tempMess[Object.keys(tempMess).length] = [eValue, "isB"];
+  localStorage.setItem("data", JSON.stringify(tempMess));
   elemScreen.scrollTop = elemScreen.scrollHeight;
+  e.target.value = "";
+  sendMessage(eValue);
 };
 input.addEventListener("change", updateValue);
 
-window.addEventListener("message", function (event) {
-  if (event.data != "") {
-    postMessage(event.data);
-  }
-
-  elemScreen.scrollTop = elemScreen.scrollHeight;
-});
-
 let Reset = () => {
   localStorage.clear();
+  location.reload();
+};
+window.addEventListener("message", function (event) {
+  if (event.data != "") {
+    sendMessage(event.data);
+  }
+});
+
+let sendMessage = (value) => {
+  var popup = window.open("http://127.0.0.1:8080/index.html", "chatA");
+  popup.postMessage(value, "http://127.0.0.1:8080/index.html");
 };
