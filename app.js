5f1b615f975efa072935669b26ef4334ad2ceb00
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2022-04-20 17:24:23 +0700

    NEW: UI color, float each people

--- app.js
+++ app.js
@@ -4,28 +4,37 @@ const elemChatA = document.getElementById("chatA");
 const input = document.querySelector("input");
 const elemScreen = document.getElementById("screen");
 
-let sendMessage = (value) => {
+let renderMess = (value, isA) => {
   let elemDiv = document.createElement("p");
   elemDiv.setAttribute("class", "col  message");
-  //   elemDiv.setAttribute("style", "display: inline-block");
+  elemDiv.setAttribute(
+    "style",
+    isA == "isA"
+      ? "background: lightblue; float: right"
+      : "background: lightcoral;float: left"
+  );
   elemDiv.innerHTML = value;
   elemChatA.appendChild(elemDiv);
 };
+let lastMess = () => {
+  elemScreen.scrollTop = elemScreen.scrollHeight;
+};
 let getMessage = () => {
   for (let key in tempMess) {
-    sendMessage(tempMess[key][0]);
+    renderMess(tempMess[key][0], tempMess[key][1]);
   }
+  lastMess();
 };
 getMessage();
 
 let updateValue = (e) => {
   let eValue = e.target.value;
-  sendMessage(eValue);
   tempMess[Object.keys(tempMess).length] = [eValue, "isA"];
   localStorage.setItem("data", JSON.stringify(tempMess));
-  elemScreen.scrollTop = elemScreen.scrollHeight;
   e.target.value = "";
-  sendMessage(eValue);
+  renderMess(eValue, "isA");
+  postMessage(eValue);
+  lastMess();
 };
 input.addEventListener("change", updateValue);
 
@@ -35,11 +44,12 @@ let Reset = () => {
 };
 window.addEventListener("message", function (event) {
   if (event.data != "") {
-    sendMessage(event.data);
+    renderMess(event.data, "isB");
   }
+  lastMess();
 });
 
-let sendMessage = (value) => {
+let postMessage = (value) => {
   var popup = window.open("http://127.0.0.1:8080/chatB.html", "index");
   popup.postMessage(value, "http://127.0.0.1:8080/chatB.html");
 };
